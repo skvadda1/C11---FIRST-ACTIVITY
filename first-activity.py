6e19d8c0a12e14718b7777e1538e773cc72f24f9
skvadda1 <156102921+skvadda1@users.noreply.github.com>
2024-10-26 10:58:05 -0400

    Add files via upload

---
+++ first-activity.py
@@ -0,0 +1,21 @@
+class Person(object):
+    def __init__(self,name,idnumber):
+        self.name = name
+        self.idnumber = idnumber
+    def display(self):
+        print(self.name)
+        print(self.idnumber)
+
+class Employee(Person):
+    def __init__(self, name, idnumber, salary,post):
+        self.salary = salary
+        self.post = post
+
+        Person.__init__(self, name, idnumber)
+
+
+
+a = Employee("Zara" , 7112022, 100000, "CEO")
+
+a.display()
+        
